da318a85285f69dd46eb9c0a4e0df84df1c2ce9d
agf30rus <gsj2t4f75b@privaterelay.appleid.com>
2026-05-30 17:14:30 +0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function showScreen(screenId) {
-    document.querySelectorAll('.screen').forEach(s => s.classList.remove('active'));
-    document.getElementById('screen-' + screenId).classList.add('active');
-}
-
-// Инициализация Telegram WebApp
-const tg = window.Telegram.WebApp;
-tg.ready();
